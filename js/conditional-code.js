579c94a55c45bc9732937e20a8fa58e4eee726b8
Leonardo Aguilera <leonardolag1268@gmail.com>
2023-03-18 12:12:42 -0600

    added warnings to input element

---
+++ js/conditional-code.js
@@ -0,0 +1,9 @@
+
+
+// let characterCount = document.querySelector('#product-name').value.length;
+// let characterCount = parseInt(document.querySelector('.character-count').textContent);
+
+if (document.querySelector('#product-name').value.length === 50) {
+    console.log(document.querySelector('#product-name').value.length)
+    // document.querySelector('.character-count').classList.add('#remaining-chars.warning');
+}
--- js/script.js
+++ js/script.js
@@ -3,7 +3,20 @@ function characterCounter(event) {
     let characterInput = event.target.value.length;
     let characterCount = 60 - characterInput;
     document.querySelector('.character-count').innerText = characterCount;
-    
+    if (characterCount === 0) {
+        document.querySelector('.character-count').classList.add('error');
+        document.getElementById('product-name').classList.add('error');
+    } else if (characterCount <= 10) {
+        document.querySelector('.character-count').classList.add('warning');
+        document.getElementById('product-name').classList.add('warning');
+        document.querySelector('.character-count').classList.remove('error');
+        document.getElementById('product-name').classList.remove('error');
+    } 
+    else {
+        document.querySelector('.character-count').classList.remove('warning');
+        document.getElementById('product-name').classList.remove('warning');
+        
+    }
 }
 
 document.querySelector('#product-name').addEventListener('input', characterCounter);
